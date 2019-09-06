77ad3c132417e02d529a3f32921f8f33bfa03bc1
aleks.marchenko <aleks.marchenko802@gmail.com>
2019-09-06 23:53:17 +0300

    add swagger

--- src/main/java/academy/softserve/flightbooking/Application.java
+++ src/main/java/academy/softserve/flightbooking/Application.java
@@ -1,4 +1,4 @@
-package academy.softserve.flightbooking.hello;
+package academy.softserve.flightbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
