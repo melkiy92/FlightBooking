9612887447f3e3354e2a5c6df01cef4a3c7ff96c
Olexii Nazarenko <olexiinazarenko@gmail.com>
2019-09-11 19:42:55 +0300

    Added explicit getters and setters to dtos

--- src/main/java/academy/softserve/flightbooking/dto/SearchCriterionDTO.java
+++ src/main/java/academy/softserve/flightbooking/dto/SearchCriterionDTO.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @Builder
-@NoArgsConstructor
+//@NoArgsConstructor
 @AllArgsConstructor
 public class SearchCriterionDTO {
     private Long id;
@@ -22,4 +22,87 @@ public class SearchCriterionDTO {
     private String toLocation;
     private LocalDate DepartDate;
     private LocalDate ReturnDate;
+
+    public SearchCriterionDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public void setCurrencyCode(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public TicketType getTicketType() {
+        return ticketType;
+    }
+
+    public void setTicketType(TicketType ticketType) {
+        this.ticketType = ticketType;
+    }
+
+    public CabinClass getCabinClass() {
+        return cabinClass;
+    }
+
+    public void setCabinClass(CabinClass cabinClass) {
+        this.cabinClass = cabinClass;
+    }
+
+    public Integer getAdults() {
+        return adults;
+    }
+
+    public void setAdults(Integer adults) {
+        this.adults = adults;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+
+    public String getFromLocation() {
+        return fromLocation;
+    }
+
+    public void setFromLocation(String fromLocation) {
+        this.fromLocation = fromLocation;
+    }
+
+    public String getToLocation() {
+        return toLocation;
+    }
+
+    public void setToLocation(String toLocation) {
+        this.toLocation = toLocation;
+    }
+
+    public LocalDate getDepartDate() {
+        return DepartDate;
+    }
+
+    public void setDepartDate(LocalDate departDate) {
+        DepartDate = departDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return ReturnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        ReturnDate = returnDate;
+    }
 }
--- src/main/java/academy/softserve/flightbooking/dto/TicketDTO.java
+++ src/main/java/academy/softserve/flightbooking/dto/TicketDTO.java
@@ -15,4 +15,44 @@ public class TicketDTO {
     private String bookingToken; //buy ticket
     private String provider;     //kiwi or skyscanner
     private List<RouteDTO> routes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getBookingToken() {
+        return bookingToken;
+    }
+
+    public void setBookingToken(String bookingToken) {
+        this.bookingToken = bookingToken;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public List<RouteDTO> getRoutes() {
+        return routes;
+    }
+
+    public void setRoutes(List<RouteDTO> routes) {
+        this.routes = routes;
+    }
 }
