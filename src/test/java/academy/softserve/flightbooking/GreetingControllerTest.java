5da201cc9ea5f1d5e9f202447bd424c09b8a0fa4
Olexii Nazarenko <olexiinazarenko@gmail.com>
2019-09-14 10:34:47 +0300

    resolved pom.xml conflicts

---
+++ src/test/java/academy/softserve/flightbooking/GreetingControllerTest.java
@@ -0,0 +1,13 @@
+package java.academy.softserve.flightbooking;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class GreetingControllerTest {
+
+    @Test
+    public void greeting() {
+        assertTrue(true);
+    }
+}
