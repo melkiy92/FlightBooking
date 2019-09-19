2025873a291eaa2c40aa4c5920e98b31da7ff80d
makmr <makmr11@gmail.com>
2019-09-19 18:38:04 +0300

    FLIG-23: added full paths for importing

--- src/main/java/academy/softserve/flightbooking/dto/FlightDTO.java
+++ src/main/java/academy/softserve/flightbooking/dto/FlightDTO.java
@@ -1,7 +1,11 @@
 package academy.softserve.flightbooking.dto;
 
 import academy.softserve.flightbooking.models.components.CabinClass;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Getter
--- src/main/java/academy/softserve/flightbooking/models/tickets/Flight.java
+++ src/main/java/academy/softserve/flightbooking/models/tickets/Flight.java
@@ -8,7 +8,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 
 @Data
