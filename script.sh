909bf1264625c08864220e7c3deb8b94d50e922d
MrScors <123.18.1.pavlov@gmail.com>
2019-09-27 19:37:49 +0300

    changed port of db to 3307, NEVER change in again!

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 # run proxy to connect to remote db
-./cloud_sql_proxy -dir=/cloudsql -instances=wise-arena-250513:europe-west3:flight-booking=tcp:3306 -credential_file=Json.json &
+./cloud_sql_proxy -dir=/cloudsql -instances=wise-arena-250513:europe-west3:flight-booking=tcp:3307 -credential_file=Json.json &
 
 # run .jar file
 java -jar flight-booking.jar;
