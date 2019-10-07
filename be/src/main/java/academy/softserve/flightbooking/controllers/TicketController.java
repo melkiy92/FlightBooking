7bc2c32f7330363b23171601bce78bc83ca3ea7d
Anna Trubaieva <melkiy9222@gmail.com>
2019-10-07 19:28:18 +0300

    FLIG-8: added SearchCriterionComponent

--- be/src/main/java/academy/softserve/flightbooking/controllers/TicketController.java
+++ be/src/main/java/academy/softserve/flightbooking/controllers/TicketController.java
@@ -19,11 +19,12 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.SortDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = { "http://localhost:3000" })
 @Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @RestController
